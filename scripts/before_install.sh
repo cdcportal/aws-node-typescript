c68a8b00bd3e05e90eb6128b543557ce9b4071ea
cdcportal <cdc_portal@iitism.ac.in>
2023-03-14 10:36:15 +0530

    Rectified scripts/before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,9 +7,9 @@ nvm install 16
 
 # create our working directory if it doesn't exist
 DIR="/home/ec2-user/aws-node-ts-backend"
-if [-d "$DIR"]; then
-    echo "${DIR} exists"
+if [ -d "$DIR" ]; then
+  echo "${DIR} exists"
 else
-    echo "Creating ${DIR} directory"
-    mkdir ${DIR}
+  echo "Creating ${DIR} directory"
+  mkdir ${DIR}
 fi
